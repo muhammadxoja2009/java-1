c91e789beb9a8e3a6307ec3e7ce57e2a11a33f7d
muhammadxoja2009 <karimovmuhammadxoja480@gmail.com>
2025-05-08 14:00:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+alert(
+    "welcome"
+)
+let a = prompt(
+   "FIRST"
+)
+let b = prompt(
+   "NAME"
+)
+let c = prompt(
+    "USERNAME"
+)
+let d = prompt(
+    "from"
+)
+let f = prompt(
+    "you old"
+)
+console.log(`${a}\n${b}\n ${c}\n ${d}\n ${f}\n`);
